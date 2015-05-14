b00ba23afd629813176894bb98f1522fae52f637
Reza Aghideh <reza_aghideh@aol.com>
2015-05-15 01:46:30 +0200

    First Commit

---
+++ IChattyGroup.java
@@ -0,0 +1,14 @@
+public interface
+        IChattyGroup extends IChattyGroupSubject {
+
+	/**
+	 * @return
+	 */
+	public String getGroupID();
+
+	/**
+	 * @param msg
+	 */
+	public void sendMessage(ChattyMessage msg);
+
+}
---
+++ Pva.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Reza on 13.05.2015.
+ */
+public class Pva {
+    public Pva(ChattyServer server)
+    {
+
+    }
+
+    public void deliverMessage (ChattyMessage message)
+    {
+
+    }
+
+    public void publishGroup(IChattyGroup group)
+    {
+
+    }
+
+    public void revokeGroup(IChattyGroup group)
+    {
+
+    }
+
+}
